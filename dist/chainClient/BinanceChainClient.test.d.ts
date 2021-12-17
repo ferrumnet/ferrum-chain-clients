e398deb8cec40f32e7f60053e77f0799a4c30554
Naiem Yeganeh <naiem@Naiems-MacBook-Pro.local>
2021-12-16 18:21:59 -0700

    build

--- dist/chainClient/BinanceChainClient.test.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=BinanceChainClient.test.d.ts.map
--- dist/chainClient/binance/BinanceTxParser.d.ts
+++
@@ -1,7 +0,0 @@
-import { SimpleTransferTransaction } from "../types";
-import { HexString } from "ferrum-plumbing";
-export declare class BinanceTxParser {
-    static parseFromHex(network: string, feeCurrency: string, hex: HexString, blockTime: number, hash: string): SimpleTransferTransaction | undefined;
-    private static parseBnbTrabnsaction;
-}
-//# sourceMappingURL=BinanceTxParser.d.ts.map
